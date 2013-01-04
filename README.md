c9ab8059049a346d1b3491cb62fb125d4d1c2362
hbddingding <hebiduhebi@126.com>
2013-01-04 16:03:42 +0800

    initialize the project.not complete.
    Signed-off-by: hbddingding <hebiduhebi@126.com>

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 VideoSite_MVC3_EF4
 ==================
 
-ASP.NET MVC3 website with EF4 .
+* ASP.NET MVC3 website with EF4 .
---
+++ Respotity/IRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Respotity
+{
+    interface IRepository<T> where T:class
+    {
+        IEnumerable<T> FindAll(Func<T, bool> exp);
+        void Add(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
